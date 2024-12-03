fbd9acc334a95eb01fdda59f299f85dd3fafa1a7
kcz358 <kaichenzhang358@outlook.com>
2024-12-03 13:15:08 +0000

    Add train utils

--- src/synvo_engine/utils/train/train_utils.py
+++ src/synvo_engine/utils/train/train_utils.py
@@ -1,4 +1,18 @@
 class TrainUtilities:
     @staticmethod
-    def prepare_model(self):
+    def prepare_model():
         pass
+
+    @staticmethod
+    def convert_open_to_hf(messages):
+        hf_messages = []
+        for message in messages:
+            new_message = {"role": message["role"], "content": []}
+            for content in message["content"]:
+                if content["type"] == "image_url":
+                    new_message["content"].append({"type": "image"})
+                else:
+                    new_message["content"].append(content)
+            hf_messages.append(new_message)
+
+        return hf_messages
