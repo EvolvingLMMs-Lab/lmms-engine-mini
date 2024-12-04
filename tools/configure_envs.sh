91829399429dc1a768cdb3d4ed438487f1eb7102
kcz358 <kaichenzhang358@outlook.com>
2024-12-04 10:53:49 +0000

    update

---
+++ tools/configure_envs.sh
@@ -0,0 +1,11 @@
+
+git clone https://github.com/NVIDIA/apex
+cd apex
+# pip install -v --disable-pip-version-check --no-cache-dir --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+cd ..
+
+git clone https://github.com/NVIDIA/Megatron-LM.git
+cd Megatron-LM
+git checkout core_r0.5.0
+pip install --no-use-pep517 -e .
