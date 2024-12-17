d86ab7a0bac7b92b4fc16ca09edb7dc3623957e9
kcz358 <kaichenzhang358@outlook.com>
2024-12-17 05:01:41 +0000

    Support load from mm_projector

--- src/synvo_engine/train/base_trainer.py
+++ src/synvo_engine/train/base_trainer.py
@@ -5,6 +5,7 @@ from loguru import logger
 
 from ..datasets import DatasetFactory
 from ..models import ModelFactory
+from ..utils.train import TrainUtilities
 from .config import TrainerConfig
 
 
@@ -21,6 +22,8 @@ class BaseTrainer(ABC):
     def build(self):
         self.model = self._build_model()
         self.train_dataset = self._build_train_dataset()
+        if self.model_config.pretrain_mm_mlp_adapter is not None:
+            self._load_mm_projector()
 
     def _build_model(self):
         model_class = ModelFactory.create_model(self.model_config.model_class)
@@ -34,6 +37,36 @@ class BaseTrainer(ABC):
                 setattr(model.config, key, value)
                 logger.info(f"Overwrite {key} to {value}")
         return model
+
+    def _load_mm_projector(self):
+        pretrain_mm_mlp_adapter = self.config.model_config.pretrain_mm_mlp_adapter
+        mm_projector_weights = torch.load(pretrain_mm_mlp_adapter, map_location="cpu")
+
+        def get_w(weights, keyword):
+            return {
+                k.split(keyword + ".")[1]: v for k, v in weights.items() if keyword in k
+            }
+
+        deepspeed3_enabled = hasattr(
+            [p for p in self.model.multi_modal_projector.parameters()][0], "ds_id"
+        )
+
+        TrainUtilities.load_zero_partitions(
+            self.model.multi_modal_projector,
+            get_w(mm_projector_weights, "multi_modal_projector"),
+            deepspeed3_enabled,
+            pretrain_mm_mlp_adapter,
+        )
+        TrainUtilities.load_zero_partitions(
+            self.model.audio_modal_projector,
+            get_w(mm_projector_weights, "audio_modal_projector"),
+            deepspeed3_enabled,
+            pretrain_mm_mlp_adapter,
+        )
+
+        logger.info(
+            f"Loaded multi_modal_projector,audio_modal_projector weights from {pretrain_mm_mlp_adapter}."
+        )
 
     def _build_train_dataset(self):
         dataset = DatasetFactory.create_dataset(self.train_dataset_config)
