09730a67e269c645911338c916ffd8fcacf0f802
kcz358 <kaichenzhang358@outlook.com>
2024-12-17 04:32:06 +0000

    Add _save_checkpoints, comment out self.state re init

--- src/synvo_engine/train/custom/llava_trainer.py
+++ src/synvo_engine/train/custom/llava_trainer.py
@@ -336,6 +336,30 @@ class LLaVATrainer(Trainer):
                 self.args, self.state, self.control
             )
 
+    def _save_checkpoint(self, model, trial, metrics=None):
+        if getattr(self.args, "only_save_mm_adapter", False):
+            from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
+
+            checkpoint_folder = f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}"
+
+            run_dir = self._get_output_dir(trial=trial)
+            output_dir = os.path.join(run_dir, checkpoint_folder)
+
+            # Only save Adapter
+            keys_to_match = ["multi_modal_projector", "audio_modal_projector"]
+
+            weight_to_save = TrainUtilities.get_mm_adapter_state_maybe_zero_3(
+                self.model.named_parameters(), keys_to_match
+            )
+
+            if self.args.local_rank == 0 or self.args.local_rank == -1:
+                self.model.config.save_pretrained(output_dir)
+                torch.save(
+                    weight_to_save, os.path.join(output_dir, f"mm_projector.bin")
+                )
+        else:
+            super(LLaVATrainer, self)._save_checkpoint(model, trial)
+
     def training_step(
         self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]
     ):
@@ -532,7 +556,7 @@ class LLaVATrainer(Trainer):
         if not delay_optimizer_creation:
             self.create_optimizer_and_scheduler(num_training_steps=max_steps)
 
-        self.state = TrainerState()
+        # self.state = TrainerState()
         self.state.is_hyper_param_search = trial is not None
         self.state.train_batch_size = self._train_batch_size
 
