df4dddc3d142a134a2a9bea28c0899bcd0d737ad
kcz358 <kaichenzhang358@outlook.com>
2025-01-02 11:51:47 +0000

    Refactor preference dataset

--- src/synvo_engine/datasets/preference_dataset.py
+++ src/synvo_engine/datasets/preference_dataset.py
@@ -1,13 +1,22 @@
 from torch.utils.data import Dataset
 
+from .base_dataset import BaseDataset
 from .config import DatasetConfig
 from .processor import ProcessorConfig
 
 
-class TextPreferenceDataset(Dataset):
-    def __init__(self, config: DatasetConfig) -> None:
-        super().__init__()
-        self.config = config
-        self.processor_config = config.processor_config
-        if isinstance(self.processor_config, dict):
-            self.processor_config = ProcessorConfig(**self.processor_config)
+class VisionPreferenceDataset(BaseDataset):
+    def load_from_hf(self, data):
+        raise NotImplementedError
+
+    def load_from_json(self, data, data_folder=None):
+        raise NotImplementedError
+
+    def __getitem__(self, index):
+        return super().__getitem__(index)
+
+    def __len__(self):
+        return super().__len__()
+
+    def get_collator(self):
+        return super().get_collator()
