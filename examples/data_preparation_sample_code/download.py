384627dfb01ed55d1297e0d64783ceb88822a1ad
Fanyi Pu <FPU001@e.ntu.edu.sg>
2025-01-03 21:54:47 +0800

    Refactor download script for improved readability and structure

--- examples/data_preparation_sample_code/download.py
+++ examples/data_preparation_sample_code/download.py
@@ -1,10 +1,14 @@
-from huggingface_hub import snapshot_download
-from pathlib import Path
 import tarfile
+from pathlib import Path
 
+from huggingface_hub import snapshot_download
 
 if __name__ == "__main__":
-    data_path = snapshot_download(repo_id="Evo-LMM/rlaif-v", repo_type="dataset", local_dir="/data/pufanyi/project/synvo_engine/playground/data/Evo-LMM/rlaif-v")
+    data_path = snapshot_download(
+        repo_id="Evo-LMM/rlaif-v",
+        repo_type="dataset",
+        local_dir="/data/pufanyi/project/synvo_engine/playground/data/Evo-LMM/rlaif-v",
+    )
     data_path = Path(data_path)
     for image_zip in data_path.glob("*.tar.gz"):
         with tarfile.open(image_zip, "r:gz") as tar:
