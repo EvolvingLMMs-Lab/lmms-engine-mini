aa99d4b03b2fce947c30fead270eec736a034fcc
Kaichen Zhang - NTU <kaichenzhang358@outlook.com>
2025-01-07 14:10:12 +0800

    [Fix] LLaVA-OV chat template masking (#6)
    * Fix masking in kino
    
    * Update test
    
    ---------
    
    Co-authored-by: brian.li <brian.li@bytedance.com>

--- src/synvo_engine/datasets/processor/vision_audio/kino_processor.py
+++ src/synvo_engine/datasets/processor/vision_audio/kino_processor.py
@@ -154,6 +154,8 @@ class KinoDataProcessor:
             if role in ["user", "system"]:
                 target += [-100] * len(encode_id)
             else:
+                # Adopted from llava-ov that mask out the assistant
+                encode_id[:3] = [-100] * 3
                 target += encode_id
 
         assert len(input_id) == len(target), f"{len(input_id)} != {len(target)}"
--- test/dataset/test_vision_audio_data.py
+++ test/dataset/test_vision_audio_data.py
@@ -1,3 +1,5 @@
+import os
+import pathlib
 import unittest
 
 from torch.utils.data import DataLoader
@@ -5,15 +7,20 @@ from torch.utils.data import DataLoader
 from synvo_engine.datasets import DatasetConfig, DatasetFactory
 from synvo_engine.utils.train import TrainUtilities
 
+current_dir = pathlib.Path().resolve()
+data_folder = current_dir.parent.parent / "examples" / "sample_jsonl_data"
+
 
 class TestVisionDataset(unittest.TestCase):
     def test_sft_dataset(self):
         config = {
             "dataset_type": "vision_audio",
             "dataset_format": "jsonl",
-            "dataset_path": "./examples/sample_jsonl_data/voice_assis/voice_assis.jsonl",
+            "dataset_path": os.path.join(
+                str(data_folder), "voice_assis", "voice_assis.jsonl"
+            ),
             "processor_config": {
-                "processor_name": "kcz358/kino-7b-init",
+                "processor_name": "Evo-LMM/kino-7b-init",
                 "processor_modality": "vision_audio",
                 "processor_type": "kino",
             },
@@ -23,13 +30,16 @@ class TestVisionDataset(unittest.TestCase):
         dataset = DatasetFactory.create_dataset(dataset_config)
         dataset.build()
         collator = dataset.get_collator()
-        dataLoader = DataLoader(
-            dataset, batch_size=4, shuffle=False, collate_fn=collator
+        outputs = dataset.load_from_json(
+            dataset.data_list[0], data_folder=str(current_dir.parent.parent)
         )
-        for data in dataLoader:
-            # TrainUtilities.sanity_check_labels(dataset.processor.processor, data["input_ids"], data["labels"])
-            print([f"{key}: {value.shape}" for key, value in data.items()])
-            break
+        # dataLoader = DataLoader(
+        # dataset, batch_size=4, shuffle=False, collate_fn=collator
+        # )
+        # for data in dataLoader:
+        # # TrainUtilities.sanity_check_labels(dataset.processor.processor, data["input_ids"], data["labels"])
+        # print([f"{key}: {value.shape}" for key, value in data.items()])
+        # break
 
 
 if __name__ == "__main__":
