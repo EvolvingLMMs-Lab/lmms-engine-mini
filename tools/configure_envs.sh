6bbd83759f69c19eebde4efab9e2f0421f9c3b91
kcz358 <kaichenzhang358@outlook.com>
2025-03-18 08:10:36 +0000

    Update megatron env settings

--- tools/configure_envs.sh
+++ tools/configure_envs.sh
@@ -1,13 +1,15 @@
 
 git clone https://github.com/NVIDIA/apex
 cd apex
+python3 -m pip install packagine
 # pip install -v --disable-pip-version-check --no-cache-dir --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
-python3 -m pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+python3 setup.py install
 cd ..
 
 git clone https://github.com/NVIDIA/Megatron-LM.git
 cd Megatron-LM
 git checkout core_r0.5.0
+python3 -m pip install pybind11
 python3 -m pip install --no-use-pep517 -e .
 
-python3 -m pip install git+https://github.com/NVIDIA/TransformerEngine.git@stable
+python3 -m pip install -v git+https://github.com/NVIDIA/TransformerEngine.git@stable
