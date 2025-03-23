c586f2b8e32523d4b390ca2babe5121b5dd3029f
Kaichen Zhang - NTU <kaichenzhang358@outlook.com>
2025-03-23 17:08:16 +0800

    [Feat] Add mistral (#16)
    * Init modeling mistral audio files
    
    * Add init file
    
    * Prepare mistral audio script
    
    * Fix mistral audio processing and forward error
    
    * Add into model factor
    
    * Init data processor
    
    * Fix mistral chat template
    
    * Add attn restrictions

--- src/lmms_engine/datasets/processor/vision_audio/__init__.py
+++ src/lmms_engine/datasets/processor/vision_audio/__init__.py
@@ -1,4 +1,9 @@
 from .kino_processor import KinoDataProcessor
 from .kino_qwen2_5_vl import KinoQwen2_5_DataProcessor
+from .mistral3_audio import Mistral3AudioDataProcessor
 
-__all__ = ["KinoDataProcessor", "KinoQwen2_5_DataProcessor"]
+__all__ = [
+    "KinoDataProcessor",
+    "KinoQwen2_5_DataProcessor",
+    "Mistral3AudioDataProcessor",
+]
---
+++ src/lmms_engine/models/mistral3_audio/__init__.py
@@ -0,0 +1,9 @@
+from .configuration_mistral3_audio import Mistral3AudioConfig
+from .modeling_mistral3_audio import Mistral3AudioForConditionalGeneration
+from .processing_mistral3_audio import Mistral3AudioProcessor
+
+__all__ = [
+    "Mistral3AudioConfig",
+    "Mistral3AudioProcessor",
+    "Mistral3AudioForConditionalGeneration",
+]
