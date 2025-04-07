52c8a671d1508d63793603d5c48345dd8f00dd61
kcz358 <kaichenzhang358@outlook.com>
2025-04-07 09:04:54 +0000

    Add get common training flops

--- src/lmms_engine/utils/train_utils.py
+++ src/lmms_engine/utils/train_utils.py
@@ -112,6 +112,147 @@ class TrainUtilities:
             param = param.detach().cpu().clone()
         return param
 
+    @staticmethod
+    def get_attn_flops(
+        num_layers,
+        hidden_size,
+        num_heads,
+        num_key_value_heads,
+        seq_len,
+        ffn_hidden_size,
+        batch_size=1,
+    ):
+        """Counts flops for the attention part for a model
+        Args:
+            num_layers: number of decoder layers
+            hidden_size: hidden size of the model
+            num_heads: number of heads in the model
+            num_key_value_heads: number of key/value heads in the model
+            ffn_hidden_size: hidden size of the FFN
+            seq_len: sequence length of the decoder
+            batch_size: batch size
+        Returns:
+            model_flops: flops in the model (should be independent of the hardware and model implementation)
+        """
+        if num_key_value_heads is None:
+            num_key_value_heads = num_heads
+        hidden_size_per_head = hidden_size // num_heads
+        # In the following we mark the reduced dimension with parentheses
+        # decoder
+        # self attention
+        ## qkv projection
+        qkv_proj_flops_fwd = (
+            2
+            * num_layers
+            * batch_size
+            * seq_len
+            * (hidden_size)
+            * num_heads
+            * hidden_size_per_head
+            + 2
+            * num_layers
+            * batch_size
+            * seq_len
+            * (hidden_size)
+            * 2
+            * num_key_value_heads
+            * hidden_size_per_head
+        )
+        ## qk logits
+        qk_logits_flops_fwd = (
+            2
+            * num_layers
+            * batch_size
+            * num_heads
+            * seq_len
+            * (hidden_size_per_head)
+            * seq_len
+        )
+        ## v logits
+        v_logits_flops_fwd = (
+            2
+            * num_layers
+            * batch_size
+            * num_heads
+            * seq_len
+            * (seq_len)
+            * hidden_size_per_head
+        )
+        ## attn out
+        attn_out_flops_fwd = (
+            2
+            * num_layers
+            * batch_size
+            * num_heads
+            * seq_len
+            * (hidden_size_per_head)
+            * hidden_size
+        )
+        # FF
+        ## 1st layer
+        ffn_1_flops_fwd = (
+            4 * num_layers * batch_size * seq_len * (hidden_size) * ffn_hidden_size
+        )
+        ## 2nd layer
+        ffn_2_flops_fwd = (
+            2 * num_layers * batch_size * seq_len * (ffn_hidden_size) * hidden_size
+        )
+
+        flops_fwd = (
+            qkv_proj_flops_fwd
+            + qk_logits_flops_fwd
+            + v_logits_flops_fwd
+            + attn_out_flops_fwd
+            + ffn_1_flops_fwd
+            + ffn_2_flops_fwd
+        )
+        return flops_fwd
+
+    @staticmethod
+    def get_decoder_flops(
+        num_layers,
+        hidden_size,
+        num_heads,
+        num_key_value_heads,
+        vocab_size,
+        seq_len,
+        ffn_hidden_size,
+        batch_size=1,
+    ):
+        """Counts flops in an decoder-only model
+        Args:
+            num_layers: number of decoder layers
+            hidden_size: hidden size of the model
+            num_heads: number of heads in the model
+            num_key_value_heads: number of key/value heads in the model
+            ffn_hidden_size: hidden size of the FFN
+            vocab_size: size of the vocabulary
+            seq_len: sequence length of the decoder
+            batch_size: batch size
+        Returns:
+            model_flops: flops in the model (should be independent of the hardware and model implementation)
+        """
+        decoder_flops_fwd = TrainUtilities.get_attn_flops(
+            num_layers,
+            hidden_size,
+            num_heads,
+            num_key_value_heads,
+            seq_len,
+            ffn_hidden_size,
+            batch_size=batch_size,
+        )
+
+        # lm head
+        lm_head_flops_fwd = 2 * batch_size * seq_len * (hidden_size) * vocab_size
+
+        # the bwd pass requires double the flops in case of matmuls to calculate the gradients with respect to
+        # both input and weight tensors
+        model_flops = 3 * (
+            decoder_flops_fwd + lm_head_flops_fwd
+        )  # 1 for fwd + 2 for bwd
+
+        return model_flops
+
     # Copied from https://github.com/baaivision/EVA/blob/master/EVA-CLIP-18B/shinji/eva_clip/factory.py#L168
     @staticmethod
     def load_zero_partitions(
